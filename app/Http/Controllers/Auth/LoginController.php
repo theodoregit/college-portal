003a29bc035a851f5f6aaf2feaefd6742dae3446
theodoregit <tedoteams@gmail.com>
2021-03-01 10:49:13 +0300

    some directories and files are added

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/portals/student/index';
 
     /**
      * Create a new controller instance.
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,39 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+//Student routes
+Route::get('/portals/student/index', [
+    'uses' => 'StudentsController@index',
+    'as' => 'portals.student.index'
+]);
+
+Route::get('/portals/student/announcement', [
+    'uses' => 'StudentsController@announcement',
+    'as' => 'portals.student.announcement'
+]);
+
+Route::get('/portals/student/grade', [
+    'uses' => 'StudentsController@grade',
+    'as' => 'portals.student.grade'
+]);
+
+Route::get('/portals/student/profile', [
+    'uses' => 'StudentsController@profile',
+    'as' => 'portals.student.profile'
+]);
+
+Route::get('/portals/student/register', [
+    'uses' => 'StudentsController@register',
+    'as' => 'portals.student.register'
+]);
+
+Route::get('/portals/student/report', [
+    'uses' => 'StudentsController@report',
+    'as' => 'portals.student.report'
+]);
